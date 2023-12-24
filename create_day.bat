0319f7c6ef043879016846a824c2b25489fba102
Kerryn <example@example.com>
2023-12-24 21:49:09 +1100

    fix create_day

--- create_day.bat
+++ create_day.bat
@@ -1,5 +1,5 @@
 mkdir src\inputs\day_%1
-echo. > src\inputs\day_%1\input_example_.txt
-echo. > src\inputs\day_%1\input_example_.txt
+echo. > src\inputs\day_%1\input_example_1.txt
+echo. > src\inputs\day_%1\input.txt
 
 copy src\day_x.rs src\day_%1.rs
