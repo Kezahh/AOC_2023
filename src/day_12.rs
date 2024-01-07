0909a27279ea45ab5011c37927880296a16e29f6
Kerryn <example@example.com>
2024-01-07 13:21:11 +1100

    day_12 finished

--- src/day_12.rs
+++ src/day_12.rs
@@ -2,8 +2,9 @@ const INPUTS_FOLDER: &str = "src/inputs/day_12";
 
 #[warn(dead_code)]
 use crate::generic;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 enum SpringState {
     Operational,
     Damaged,
@@ -28,7 +29,7 @@ impl SpringState {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 struct SpringRecord {
     spring_map: Vec<usize>,
     spring_mask: Vec<char>,
@@ -104,97 +105,118 @@ impl SpringRecord {
         return gaps_set;
     }
 
-    fn get_possibilities2(&self) -> usize {
-        let gaps_set: HashSet<Vec<usize>> = self.get_gap_set();
-        let mut possibilities: HashSet<Vec<char>> = HashSet::new();
+    fn get_possibilities(&self, possibility_cache: &mut HashMap<SpringRecord, usize>) -> usize {
+        /* 
+         * Algorithm logic from manhunto
+         * https://github.com/manhunto/advent-of-code-rs/blob/master/src/solutions/day12.rs
+         * 
+         */
 
-        for gaps in gaps_set {
-            //println!("\tgaps = {:?}", gaps);
-            let mut possibility: Vec<char> = Vec::new();
-            for i in 0..self.spring_map.len() {
-                if i == 0 {
-                    possibility.append(&mut vec![SpringState::Operational.as_char(); gaps[i]]);
-                } else {
-                    // gaps in between get an extra 1 as they always need to be a gap.
-                    // Only the first and last gap can be 0.
-                    possibility.append(&mut vec![SpringState::Operational.as_char(); gaps[i] + 1]);
-                }
-                possibility.append(&mut vec![SpringState::Damaged.as_char(); self.spring_map[i]]);
-            }
-            possibility.append(&mut vec![SpringState::Operational.as_char(); *gaps.last().expect("Gaps is empty")]);
 
-            let mut possibility_with_mask = possibility.clone();
-            for i in 0..self.spring_mask.len() {
-                if self.spring_mask[i] == SpringState::Unknown.as_char() {
-                    possibility_with_mask[i] = SpringState::Unknown.as_char();
-                }
-            }
-            //println!("\tpossibility = {:?}", possibility);
+        // println!("\tGetting possibilities for {:?} {:?}", self.spring_mask, self.spring_map);
 
-            if possibility_with_mask == self.spring_mask {
-                //println!("\tpossibility = {:?}", possibility);
-                possibilities.insert(possibility);
-                //println!("\t\tyep");
-            }
+
+        // Check if answer already exists in cache.
+        if let Some(result) = possibility_cache.get(self) {
+            // println!("\t\talready exists {:?} {:?}", self.spring_mask, self.spring_map);
+            // println!("\t\t\tpossibilities = {}", result);
+            return *result;
         }
         
 
-        return possibilities.len();
-    }
+        // check if map is empty.
+        if self.spring_map.len() == 0 {
+            let result: usize;
 
-    fn get_possibilities(&self) -> usize {
-        let mut wild_places: Vec<usize> = Vec::new();
-        for i in 0..self.spring_mask.len() {
-            if self.spring_mask[i] == '?' {
-                wild_places.push(i);
+            // if the mask contains any damaged springs then it is not possible as the 
+            // map is empty. No more damanged springs.
+            if self.spring_mask.contains(&SpringState::Damaged.as_char()) {
+                result = 0;
+            } else {
+                result = 1;
             }
-        }
-        // let mut wilds_on_off: Vec<bool> = vec![false; wild_places.len()];
 
-        // let mut all_possibilities: Vec<Vec<usize>> = Vec::new();
-        // let total_loops: usize = (2 as i32).pow(wilds_on_off.len() as u32) as usize;
+            possibility_cache.insert(self.clone(), result);
 
-        // for x in 0..total_loops {
-        //     if x > 0 {
-        //         for y in 0..wilds_on_off.len() {
-        //             if x % ((2 as i32).pow(y as u32)) as usize == 0 {
-        //                 wilds_on_off[y] = !wilds_on_off[y];
-        //             }
-        //         }
-        //     }
+            // println!("\t\tspring map is empty {:?} {:?}", self.spring_mask, self.spring_map);
+            // println!("\t\t\tpossibilities = {}", result);
 
-        //     //println!("\t{:?} x = {}", gaps, x);
-        //     all_possibilities.push(wilds_on_off.iter().map(|x| *x as usize).collect::<Vec<usize>>());
-        // }
-
-        let total_loops: usize = (2 as i32).pow(wild_places.len() as u32) as usize;
-        let mut spring_possibilities: Vec<String> = Vec::new();
-
-        for x in 0..total_loops {
-            let mut possibility: Vec<char> = self.spring_mask.clone();
-            for i in 0..wild_places.len() {
-                if (x >> i) & 1 == 1 {
-                    possibility[wild_places[i]] = SpringState::Damaged.as_char();
-                } else {
-                    possibility[wild_places[i]] = SpringState::Operational.as_char();
-                }
-            }
-            let possibility_string: String = String::from_iter(possibility.iter());
-            //println!("\t{:?}", possibility_string);
-            let spring_map = possibility_string.split(SpringState::Operational.as_char())
-                .filter(|x| x.len() != 0)
-                .map(|x| x.len())
-                .collect::<Vec<usize>>();
-
-            //println!("\t\t\t{:?}", spring_map);
-            if spring_map == self.spring_map {
-                spring_possibilities.push(possibility_string);
-                //println!("\t{:?}", possibility);
-            }
-            
+            return result;
         }
 
-        return spring_possibilities.len();
+
+        // Check if there are enough springs left for group.
+        if self.spring_mask.len() < self.get_min_length() {
+            possibility_cache.insert(self.clone(), 0);
+
+            // println!("\t\tSpring mask is too small {:?} {:?}", self.spring_mask, self.spring_map);
+            // println!("\t\t\tpossibilities = 0");
+
+            return 0;
+        }
+
+
+        // Check if first spring is operational.
+        // If it is, then skip ahead one character in the mask and same spring map.
+        let first_spring = SpringState::from_char(self.spring_mask[0]);
+        if first_spring == SpringState::Operational {
+            let new_spring_record = Self {
+                spring_map: self.spring_map.clone(),
+                spring_mask: self.spring_mask[1..].to_vec(),
+            };
+
+            let result = new_spring_record.get_possibilities(possibility_cache);
+            possibility_cache.insert(self.clone(), result);
+
+            // println!("\t\tfirst is operational - skip ahead {:?} {:?}", self.spring_mask, self.spring_map);
+            // println!("\t\t\tpossibilities = {}", result);
+
+            return result;
+        }
+
+
+        let spring_count = self.spring_map[0];
+        let all_broken_or_wild = self.spring_mask[..spring_count].iter().all(|s| *s != SpringState::Operational.as_char());
+        let end_of_springs: usize;
+
+        if self.spring_mask.len() < (spring_count + 1) {
+            end_of_springs = self.spring_mask.len();
+        } else {
+            end_of_springs = spring_count + 1;
+        }
+
+
+        let mut possibilities: usize = 0;
+
+        // If all the springs in the current count are Broken or Wild
+        // and the first spring after is not-broken, then add possibilities.
+        if all_broken_or_wild {
+            if (self.spring_mask.len() > spring_count && SpringState::from_char(self.spring_mask[spring_count]) != SpringState::Damaged)
+                    || self.spring_mask.len() <= spring_count {
+                let new_spring_record = Self {
+                    spring_map: self.spring_map[1..].to_vec(),
+                    spring_mask: self.spring_mask[end_of_springs..].to_vec(),
+                };
+
+                possibilities += new_spring_record.get_possibilities(possibility_cache);
+            }
+        }
+
+        if first_spring == SpringState::Unknown {
+            let new_spring_record = Self {
+                spring_map: self.spring_map.clone(),
+                spring_mask: self.spring_mask[1..].to_vec(),
+            };
+
+            possibilities += new_spring_record.get_possibilities(possibility_cache);
+        }
+
+        possibility_cache.insert(self.clone(), possibilities);
+
+        // println!("\t\tFinished function {:?} {:?}", self.spring_mask, self.spring_map);
+        // println!("\t\t\tpossibilities = {}", possibilities);
+
+        return possibilities;
     }
 }
 
@@ -210,12 +232,14 @@ fn solve_puzzle(input_filename: String, part_2: bool) -> usize {
     let mut index: usize = 0;
 
     println!("started\n");
+
+    let mut possibility_cache: HashMap<SpringRecord, usize> = HashMap::new();
     for s in spring_records {
         //println!("gaps = {}, spare = {}", s.spring_map.len() + 1, s.get_spares());
         //println!("Spring set = {:?}", s.spring_map);
         //println!("Spring mask = {:?}", s.spring_mask);
         print!("\rdoing {}/{}", index + 1, input_lines.len());
-        total_sum += s.get_possibilities();
+        total_sum += s.get_possibilities(&mut possibility_cache);
         index += 1;
     }
     println!("\nDone");
@@ -238,13 +262,23 @@ mod tests {
         }
 
         let target_record = spring_records.first().expect("Spring records is empty??");
+        let mut possibility_cache: HashMap<SpringRecord, usize> = HashMap::new();
 
         println!("gaps = {}, spare = {}", target_record.spring_map.len() + 1, target_record.get_spares());
         println!("Spring set = {:?}", target_record.spring_map);
         println!("Spring mask = {:?}", target_record.spring_mask);
 
-        assert_eq!(target_record.get_possibilities(), answer);
+        assert_eq!(target_record.get_possibilities(&mut possibility_cache), answer);
     }
+
+    #[test]
+    fn very_quick_test() {
+        let cheeky_vec = vec!['a', 'b', 'c', 'd'];
+        let target_char: char = 'b';
+
+        assert!(cheeky_vec.contains(&target_char));
+    }
+
 
     #[test]
     fn quick_test() {
@@ -286,13 +320,13 @@ mod tests {
     fn example_2() {
         let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input_example_1.txt", true);
         println!("Answer = {:?}", answer);
-        assert!(answer == 30);
+        assert!(answer == 525152);
     }
 
     #[test]
     fn part_2() {
         let answer = solve_puzzle(INPUTS_FOLDER.to_owned() + "/input.txt", true);
         println!("Answer = {:?}", answer);
-        assert!(answer == 7185540);
+        assert!(answer == 1537505634471);
     }
 }
